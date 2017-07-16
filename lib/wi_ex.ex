6f14a135403972f90a6307f3dcd8ff98403ff9ef
Jessiah Ratliff <jessiahr@gmail.com>
2017-07-16 17:52:27 +0000

    init

---
+++ lib/wi_ex.ex
@@ -0,0 +1,18 @@
+defmodule WiEx do
+  @moduledoc """
+  Documentation for WiEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WiEx.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
